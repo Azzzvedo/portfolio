715492ffd96539b2b2e29c03a3ff18c2870daf7d
Ian Wiggins <wiggins.ian.r@gmail.com>
2022-11-20 16:44:12 +0000

    tidy homepage and add color and animation effects

---
+++ app/javascript/controllers/form_controller.js
@@ -0,0 +1,31 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="form"
+export default class extends Controller {
+  static targets = ["input", "last"]
+
+  connect() {
+  }
+
+  add() {
+    let str = this.inputTarget.value
+    this.inputTarget.value = ""
+    str = str.replace(/[^\w\s\']|_/g, "")
+      .replace(/\s+/g, " ").toLowerCase();
+    if (str.value === "") return;
+    str = str.charAt(0).toUpperCase() + str.slice(1);
+    this.lastTarget.insertAdjacentHTML('beforebegin', `
+    <div data-controller="tag">
+    <span class="tag student" data-tag-target="button">${str}.</span>
+      <div class="hidden expand" data-tag-target="expand">
+        <p>Nothing to see here!</p>
+      </div>
+    </div>`)
+  }
+
+  enter(e) {
+    if (e.key === "Enter") {
+      this.add()
+    }
+  }
+}
--- app/javascript/controllers/tag_controller.js
+++ app/javascript/controllers/tag_controller.js
@@ -2,17 +2,39 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="tag"
 export default class extends Controller {
-  static targets = ["expand"]
+  static targets = ["expand", "wiggler", "button"]
 
   connect() {
-    console.log('tag controller connected');
-  }
-
-  hide() {
-    this.expandTarget.classList.add('hidden')
+    // console.log('tag controller connected');
+    this.wiggle()
   }
 
   unhide() {
+    // console.log("fired");
     this.expandTarget.classList.remove('hidden')
+    this.buttonTarget.classList.add('button')
+
+    // this.spanTarget.classList.remove('hidden')
+  }
+
+  hide() {
+
+    this.expandTarget.classList.add('hidden')
+    this.buttonTarget.classList.remove('button')
+
+    // this.spanTarget.classList.remove('hidden')
+  }
+
+  wiggle() {
+    if (!this.hasWigglerTarget) return;
+
+    console.log(this.wigglerTarget.innerText);
+    const randomTime = this.wigglerTarget.innerText === "Ian." ? 0 : Math.floor(Math.random() * 20) + Math.random() + 1;
+    setTimeout(() => {
+      this.wigglerTarget.classList.add('wiggle')
+      setTimeout(() => {
+        this.wigglerTarget.classList.remove('wiggle')
+      }, 2000);
+    }, randomTime * 1000);
   }
 }
